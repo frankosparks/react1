5a05a8ba4fdb8f8c9fd582f590bf923b18848a31
frank <mainafrank400@gmail.com>
2024-05-10 15:30:00 +0300

    commit4

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,21 @@ import React, { Component } from "react";
 
 class Counter extends Component {
 state = {
-  count: 1
+  count: 1,
+  imageUrl: 'https://picsum.photos/200'
 };
 
   render() {
     return (
       <React.Fragment>
+        <img src={this.state.imageUrl} alt=""/>
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
   }
 }
+
 
 formatCount(); {
   const { count } = this.state;
