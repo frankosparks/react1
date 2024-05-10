c3580f6a0878fbd6a5fae91bbfd203d248c24a19
frank <mainafrank400@gmail.com>
2024-05-10 15:57:31 +0300

    commit6

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,16 +5,11 @@ state = {
   count: 1,
   };
 
-  styles = {
-    fontSize: 5,
-    fontWeight: 'bold',
-    padding: 0.05
-  }
 
   render() {
     return (
       <React.Fragment>
-        <span style={this.styles} className="badge badge-primary m-3"><h4>zero</h4></span>
+        <span style={{fontSize: 30,fontWeight:'bold'}} className="badge badge-primary m-3"><h4>zero</h4></span>
         <button className="btn btn-secondary btn-sm ">Increment</button>
       </React.Fragment>
     );
